db5e3e2c769415afc1dafdcf93e35b9234419013
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2021-08-18 13:29:38 +0600

    004

---
+++ js/banking.js
@@ -0,0 +1,6 @@
+// handle deposit button
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+    console.log(depositAmount);
+})
--- js/login.js
+++ js/login.js
@@ -5,6 +5,7 @@ document.getElementById('login-btn').addEventListener('click', function () {
     // get user password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
+    // check user password
     if (userEmail == 'demo@demo.com' && userPassword == '12345') {
         window.location.href = 'banking.html';
     }
