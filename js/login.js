e3a15e8cd194938718eff68603294fc908e2f2f4
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2021-08-18 11:25:19 +0600

    001

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('login-btn').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    if (userEmail == 'demo@demo.com' && userPassword == '12345') {
+        window.location.href = 'banking.html';
+    }
+    else {
+        alert("Please enter correct credentials");
+    }
+})
